94d2db4d3ece20c57d5f62fc9b815e54c51652c9
afif-ayoubi <afifayoubi22@gmail.com>
2024-03-01 04:16:45 +0200

    login authentication done

--- Scripts/index.js
+++ Scripts/index.js
@@ -19,3 +19,19 @@ inputs.forEach(input => {
 	input.addEventListener("focus", addcl);
 	input.addEventListener("blur", remcl);
 });
+
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+	event.preventDefault();
+	
+	var username = document.getElementById('username').value;
+	var password = document.getElementById('password').value;
+  
+	if (username === 'AdminSEF123' && password === 'SeF@ctORy$$456') {
+	
+		window.location.href = 'index.html'; 
+	  console.log('You have successfully logged in.');
+	} else {
+	  alert('Invalid username or password. Please try again!');									
+	}
+  });
+  
