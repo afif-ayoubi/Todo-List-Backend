7a7d5e283046ad47393f2982caf618f23f03aca4
afif-ayoubi <afifayoubi22@gmail.com>
2024-03-15 00:39:25 +0200

    connection to db

---
+++ connection.php
@@ -0,0 +1,10 @@
+<?php
+$host= "localhost";
+$db_user="root";
+$db_pass=null;
+$db_name="todo_list_db";
+$mysqli= new mysqli($host, $db_user, $db_pass, $db_name);
+
+if($mysqli->connect_error){
+    die("Connection failed" . $mysqli->connect_error);
+}
