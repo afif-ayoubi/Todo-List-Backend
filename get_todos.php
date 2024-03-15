089447c267806020e38d3cd06313931ca1ac860d
afif-ayoubi <afifayoubi22@gmail.com>
2024-03-15 12:36:38 +0200

    modify the get api

--- get_todos.php
+++ get_todos.php
@@ -3,7 +3,10 @@ include('connection.php');
 
 $user_id = $_GET['user_id']; 
 
-$query = $mysqli->prepare('SELECT * FROM todos WHERE user_id = ?');
+$query = $mysqli->prepare('SELECT t.*, u.username FROM todos t 
+                          LEFT JOIN users u ON t.user_id = u.id 
+                          WHERE t.user_id = ?');
+
 $query->bind_param("i", $user_id);
 $query->execute();
 $query->store_result();
@@ -13,14 +16,14 @@ if ($num_rows == 0) {
     $response['status'] = 'no todos found';
 } else {
     $todos = [];
-    $query->bind_result($id, $user_id, $description, $completed);
+    $query->bind_result($id, $user_id, $description, $completed, $username);
     while ($query->fetch()) {
         $todo = [
             'id' => $id,
             'user_id' => $user_id,
+            'username' => $username, 
             'description' => $description,
             'completed' => $completed,
-          
         ];
         $todos[] = $todo;
     }
